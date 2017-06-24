d9aa458cb2f31cec64037cac3bf9b7809c6507bf
yijiangh <yijiangh@mit.edu>
2017-06-24 10:30:07 -0400

    fix glut display problem on linux

--- src/framefab/main.cpp
+++ src/framefab/main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
+#include <GL/glut.h>
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
+	glutInit(&argc, argv);
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
